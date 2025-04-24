31d919bc155ff95007bfd808c47ad6ad937f77d3
MisterFredo <mister.fredo@gmail.com>
2025-04-24 11:45:06 +0200

    Update simulateur_core.py

--- simulateur_core.py
+++ simulateur_core.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import streamlit as st
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
 def get_type_classement(champ_id):
     query = f"""
