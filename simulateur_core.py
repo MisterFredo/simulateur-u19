7e7e95b6feed5f1450c84262376eb8849bba030f
MisterFredo <mister.fredo@gmail.com>
2025-04-24 14:00:53 +0200

    Update simulateur_core.py

--- simulateur_core.py
+++ simulateur_core.py
@@ -434,11 +434,11 @@ def recalculer_classement_simule(matchs_simules, champ_id, date_limite, selected
 
     classement["DIFF"] = classement["BP"] - classement["BC"]
 
-    # Application des pénalités
-    classement = appliquer_penalites(classement, date_limite)
-
     # Tri + numérotation
     classement = trier_et_numeroter(classement, "GENERALE")
+
+    # Application des pénalités
+    classement = appliquer_penalites(classement, date_limite)
 
     if selected_poule != "Toutes les poules":
         classement = classement[classement["POULE"] == selected_poule]
