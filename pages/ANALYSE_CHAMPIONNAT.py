47c294735e7da0bed3cc83ae34c584ee4bc2b393
MisterFredo <mister.fredo@gmail.com>
2025-05-02 23:05:06 +0200

    Update ANALYSE_CHAMPIONNAT.py

--- pages/ANALYSE_CHAMPIONNAT.py
+++ pages/ANALYSE_CHAMPIONNAT.py
@@ -22,7 +22,8 @@ from simulateur_core import (
     classement_special_u17,
     classement_special_n2,
     classement_special_n3,
-    calculer_difficulte_calendrier
+    calculer_difficulte_calendrier,
+    recalculer_classement_simule
 )
 
 # --- CONFIG STREAMLIT
